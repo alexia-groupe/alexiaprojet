6df42d6ebb66efec842619c70a43de8d00561472
alexia-groupe <alexiaentreprise@gmail.com>
2026-02-23 23:41:40 +0100

    Change background image in hero section

--- app/multiservices/page.tsx
+++ app/multiservices/page.tsx
@@ -16,10 +16,10 @@ export default function Multiservices() {
       <section className="relative h-[60vh] min-h-[500px] flex items-center justify-center bg-gray-800 text-white overflow-hidden">
         <div className="absolute inset-0 z-0">
           <Image
-            src="/multi2.jpg"
+            src="/electric.jpg"
             alt="Travaux et Maintenance"
             fill
-            className="object-cover opacity-25"
+            className="object-cover opacity-20"
           />
           <div className="absolute inset-0 bg-gray-900/40 mix-blend-multiply"></div>
         </div>
