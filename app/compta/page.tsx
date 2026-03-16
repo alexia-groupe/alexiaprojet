ad819d587acaad600016090e563aa9e406871a54
alexia-groupe <alexiaentreprise@gmail.com>
2026-03-16 18:37:00 +0100

    Increase image opacity from 200 to 300

--- app/compta/page.tsx
+++ app/compta/page.tsx
@@ -18,7 +18,7 @@ export default function Compta() {
             src="/compta.jpg"
             alt="Expertise Comptable"
             fill
-            className="object-cover opacity-200"
+            className="object-cover opacity-300"
           />
           <div className="absolute inset-0 bg-gradient-to-t from-primary via-primary/50 to-transparent"></div>
         </div>
